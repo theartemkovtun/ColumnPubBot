6c29e10d1bb6f743bb084e3f2441e88ec091d164
Hashenko <62422487+Hashenko@users.noreply.github.com>
2022-01-20 00:14:47 +0200

    fix2

--- configuration.py
+++ configuration.py
@@ -6,5 +6,5 @@ WEBHOOK_HOST = f"https://{PROJECT_NAME}.herokuapp.com"
 WEBHOOK_PATH = '/webhook/' + TOKEN
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
-MONGO_CONNECTION_STRING = 'mongodb://localhost'
+MONGO_CONNECTION_STRING = 'mongodb+srv://Vova:Vova@cluster0.nen71.mongodb.net/Claster0?retryWrites=true&w=majority'
 MONGO_DB_NAME = 'columnpubbotdb'
